ff3e1272e3ba2a548bf2e394e186f227ab641514
fedecris <federicocristina@gmail.com>
2015-09-19 11:33:48 -0300

    Finalizacion correcta de las esquinas en los angulos

--- Assets/Scripts/Init.cs
+++ Assets/Scripts/Init.cs
@@ -61,13 +61,13 @@ public class Init : MonoBehaviour {
 		// Inicializar calles
 		for (int z = 1; z <= CANT_CALLES; z++) {
 			Object calleInstance = Instantiate(callePrefab, new Vector3(CANT_AVENIDAS/2 + DESP_CALLEAV, ELEVACION_CALLEAV, (float)z), Quaternion.Euler(new Vector3(90, 0, 0)));
-			((Transform)calleInstance).localScale = new Vector3 (CANT_AVENIDAS - 1, ANCHO_CALLEAV, 1);
+			((Transform)calleInstance).localScale = new Vector3 (CANT_AVENIDAS - .75f, ANCHO_CALLEAV, 1);
 		}
 
 		// Inicializar avenidas
 		for (int x = 1; x <= CANT_AVENIDAS; x++) {
 			Object avenidaInstance = Instantiate(avenidaPrefab, new Vector3((float)x, ELEVACION_CALLEAV, CANT_CALLES/2 + DESP_CALLEAV), Quaternion.Euler(new Vector3(90, 0, 0)));
-			((Transform)avenidaInstance).localScale = new Vector3 (ANCHO_CALLEAV, CANT_CALLES - 1, 1);
+			((Transform)avenidaInstance).localScale = new Vector3 (ANCHO_CALLEAV, CANT_CALLES - .75f, 1);
 		}
 
 		// Inicializar papeles de manera aleatoria
