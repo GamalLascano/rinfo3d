51034b9cabca346b8fab1ddd75ad1b55ba594ba0
Gamal Lascano Hassen <gamallascano@gmail.com>
2020-02-26 16:57:58 -0300

    Added beta version of code checker

--- Assets/Scripts/CodeParsing.cs
+++ Assets/Scripts/CodeParsing.cs
@@ -6,7 +6,25 @@ public class CodeParsing : MonoBehaviour
 {
     bool checkCodeStructure(string[] codigo,int[] spaces)
     {
-
-        return true;
+        if (!codigo[0].Contains("programa"))
+        {
+            return false;
+        }
+        if (codigo.Length < 1) return true;
+        int j = 1;
+        //Va a buscar a comenzar o a llegar al final, lo que ocurra primero. Si llega al final, no tendria que estar indentado.
+        //Pero si para por comenzar, entraria al for, dado a que se cumple la condicion. Ahi evaluo si esta indentado o no
+        while ((codigo[j]!="comenzar") || ((codigo.Length - 1) != j))
+        {
+            j++;
+        }
+        for (int i = j; i < codigo.Length; i++)
+        {
+            if (spaces[j] < 1){
+                if (codigo[j]!="finalizar") return false;
+            }
+        }
+        if (codigo[j] == "finalizar") return true;
+        else return false;
     }
 }
