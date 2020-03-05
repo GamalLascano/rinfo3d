61078b7c387ac0caf5dd68bf3f370afd339afe19
Gamal Lascano Hassen <gamallascano@gmail.com>
2020-03-05 16:20:56 -0300

    Added condition verifier, ncalc

--- Assets/Scripts/CodeParsing.cs
+++ Assets/Scripts/CodeParsing.cs
@@ -1,10 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NCalc;
 
 public class CodeParsing : MonoBehaviour
 {
-    private const string carOP = "<>=";
+    public class ObjectListValue
+    {
+        public string Value { get; set; }
+        public string Type { get; set; }
+    }
+    private const string carOP = "!<>=&|";
+    private static int getPosCa()
+    {
+        return Mathf.RoundToInt(Init.getRobotBehaviour().getRobotPosition().z);
+    }
+    private static int getPosAv()
+    {
+        return Mathf.RoundToInt(Init.getRobotBehaviour().getRobotPosition().x);
+    }
+    private static bool HayFlorEnLaEsquina()
+    {
+        Vector3 pos = Init.getRobotBehaviour().getRobotPosition();
+        int i = Init.city[(int)pos.x - 1, (int)pos.z - 1].flowers;
+        if (i > 0) return true;
+        else return false;
+    }
+    private static bool HayFlorEnLaBolsa()
+    {
+        int i = Init.getRobotBehaviour().flores;
+        if (i > 0) return true;
+        else return false;
+    }
+    private static bool HayPapelEnLaEsquina()
+    {
+        Vector3 pos = Init.getRobotBehaviour().getRobotPosition();
+        int i = Init.city[(int)pos.x - 1, (int)pos.z - 1].papers;
+        if (i > 0) return true;
+        else return false;
+    }
+    private static bool HayPapelEnLaBolsa()
+    {
+        int i = Init.getRobotBehaviour().papeles;
+        if (i > 0) return true;
+        else return false;
+    }
     public static bool checkCodeStructure(string[] codigo,int[] spaces)
     {
         if (!codigo[0].Contains("programa"))
@@ -59,56 +99,211 @@ public class CodeParsing : MonoBehaviour
             return false;
         }
     }
-    public static ArrayList parameterProcessor(string info)
+    public int checkCondition(string frase)
     {
-        ArrayList parametros= new ArrayList();
-        ArrayList operadores = new ArrayList();
-        int markParam = 0;
-        bool startedOp = false;
         int i = 0;
-        for (i = 0; i < info.Length;i++)
+        int wordstart = 0;
+        bool startedOp = false;
+        List<ObjectListValue> parameterList = new List<ObjectListValue>();
+        for (i = 0; i < frase.Length; i++)
         {
-            if (startedOp == false)
+            if (frase[i] == '(')
             {
-                if (info[i] == '(')
+                if (startedOp == true)
                 {
-                    bool found = false;
-                    for (int j = i; j < info.Length; j++)
-                    {
-                        if (info[j] == ')') found = true;
-                    }
-                    if (found==true)
-                    {
-                        ArrayList lop = parameterProcessor(info.Substring(i, j - i));
-                        //Seguir despues, conseguir forma de sumar resultados de un arraylist en el otro
-                    }
+                    parameterList.Add(new ObjectListValue { Value = frase[i - 1].ToString(), Type = "Operator" });
+                    startedOp = false;
                 }
-                if (carOP.IndexOf(info[i]) != -1)
-                {
-                    startedOp = true;
-                }
+                int fin = frase.LastIndexOf(')');
+                string par1 = frase.Substring(i, fin + 1 - i);
+                char[] par1aux = par1.ToCharArray();
+                par1aux[0] = ' ';
+                par1aux[par1.Length - 1] = ' ';
+                par1 = new string(par1aux);
+                int valor = checkCondition(par1.Replace(" ", ""));
+                parameterList.Add(new ObjectListValue { Value = valor.ToString(), Type = "Value" });
+                i = frase.LastIndexOf(')');
+                wordstart = i;
             }
             else
             {
-                startedOp = false;
-                if (((info[i] == '=') && ((info[i - 1] == '<') || (info[i - 1] == '>')))||((info[i]=='>')&&(info[i-1]=='<')))
+                if (startedOp == true)
                 {
-                    operadores.Add(info[i - 1] + info[i]);
-                    parametros.Add(info.Substring(markParam, i - 1));
-                    markParam = i + 1;
+                    if (((frase[i] == '=') && ((frase[i - 1] == '<') || (frase[i - 1] == '>'))) || ((frase[i] == '>') && (frase[i - 1] == '<')))
+                    {
+                        parameterList.Add(new ObjectListValue { Value = (frase[i - 1] + frase[i]).ToString(), Type = "Operator" });
+                    }
+                    else
+                    {
+                        parameterList.Add(new ObjectListValue { Value = frase[i - 1].ToString(), Type = "Operator" });
+                        wordstart = i;
+                    }
+                    startedOp = false;
                 }
                 else
                 {
-                    operadores.Add(info[i - 1]);
-                    parametros.Add(info.Substring(markParam, i));
-                    markParam = i;
+                    if (carOP.IndexOf(frase[i]) != -1)
+                    {
+                        startedOp = true;
+                        if (frase[i - 1] != ')')
+                        {
+                            string par2 = frase.Substring(wordstart, i - wordstart);
+                            parameterList.Add(new ObjectListValue { Value = par2, Type = "Value" });
+                        }
+                    }
                 }
             }
         }
-        parametros.Add(info.Substring(markParam, i));
-        ArrayList res = new ArrayList();
-        res[0] = parametros;
-        res[1] = operadores;
-        return res;
+        if ((i >= frase.Length) && (frase[frase.Length - 1] != ')'))
+        {
+            string par3 = frase.Substring(wordstart, frase.Length - wordstart);
+            parameterList.Add(new ObjectListValue { Value = par3, Type = "Value" });
+        }
+        List<int> result = new List<int>();
+        int j;
+        for (int f = 0; f < parameterList.Count; f++)
+        {
+            if (parameterList[f].Type == "Value")
+            {
+                if (parameterList[f].Value.Contains("PosCa"))
+                    parameterList[f].Value = parameterList[f].Value.Replace("PosCa", getPosCa().ToString());
+                if (parameterList[f].Value.Contains("PosAv"))
+                    parameterList[f].Value = parameterList[f].Value.Replace("PosAv", getPosAv().ToString());
+                if (parameterList[f].Value.Contains("HayFlorEnLaBolsa"))
+                    parameterList[f].Value = parameterList[f].Value.Replace("HayFlorEnLaBolsa", HayFlorEnLaBolsa().ToString());
+                if (parameterList[f].Value.Contains("HayFlorEnLaEsquina"))
+                    parameterList[f].Value = parameterList[f].Value.Replace("HayFlorEnLaEsquina", HayFlorEnLaEsquina().ToString());
+                if (parameterList[f].Value.Contains("HayPapelEnLaBolsa"))
+                    parameterList[f].Value = parameterList[f].Value.Replace("HayPapelEnLaBolsa", HayPapelEnLaBolsa().ToString());
+                if (parameterList[f].Value.Contains("HayPapelEnLaEsquina"))
+                    parameterList[f].Value = parameterList[f].Value.Replace("HayPapelEnLaEsquina", HayPapelEnLaEsquina().ToString());
+            }
+        }
+        if (parameterList.Count == 1)
+        {
+            Expression help1 = new Expression(parameterList[0].Value);
+            object help2 = help1.Evaluate();
+            if ((int)help2 > 0) result.Add(1);
+            else result.Add(0);
+        }
+
+        for (j = 0; j < parameterList.Count; j++)
+        {
+            if (parameterList[j].Type == "Operator")
+            {
+                object par1;
+                object par2;
+                Expression par11;
+                Expression par22;
+                switch (parameterList[j].Value)
+                {
+                    case ">=":
+                        par11 = new Expression(parameterList[j - 1].Value);
+                        par1 = par11.Evaluate();
+                        par22 = new Expression(parameterList[j + 1].Value);
+                        par2 = par22.Evaluate();
+                        if ((int)par1 >= (int)par2)
+                        {
+                            result.Add(1);
+                        }
+                        else result.Add(0);
+                        //Voy a usar NCalc para realizar esto, integrar al proyecto el viernes
+                        break;
+                    case "<=":
+                        par11 = new Expression(parameterList[j - 1].Value);
+                        par1 = par11.Evaluate();
+                        par22 = new Expression(parameterList[j + 1].Value);
+                        par2 = par22.Evaluate();
+                        if ((int)par1 >= (int)par2)
+                        {
+                            result.Add(1);
+                        }
+                        else result.Add(0);
+                        //Voy a usar NCalc para realizar esto, integrar al proyecto el viernes
+                        break;
+                    case "<":
+                        par11 = new Expression(parameterList[j - 1].Value);
+                        par1 = par11.Evaluate();
+                        par22 = new Expression(parameterList[j + 1].Value);
+                        par2 = par22.Evaluate();
+                        if ((int)par1 < (int)par2)
+                        {
+                            result.Add(1);
+                        }
+                        else result.Add(0);
+                        //Voy a usar NCalc para realizar esto, integrar al proyecto el viernes
+                        break;
+                    case ">":
+                        par11 = new Expression(parameterList[j - 1].Value);
+                        par1 = par11.Evaluate();
+                        par22 = new Expression(parameterList[j + 1].Value);
+                        par2 = par22.Evaluate();
+                        if ((int)par1 > (int)par2)
+                        {
+                            result.Add(1);
+                        }
+                        else result.Add(0);
+                        //Voy a usar NCalc para realizar esto, integrar al proyecto el viernes
+                        break;
+                    case "=":
+                        par11 = new Expression(parameterList[j - 1].Value);
+                        par1 = par11.Evaluate();
+                        par22 = new Expression(parameterList[j + 1].Value);
+                        par2 = par22.Evaluate();
+                        if ((int)par1 == (int)par2)
+                        {
+                            result.Add(1);
+                        }
+                        else result.Add(0);
+                        //Voy a usar NCalc para realizar esto, integrar al proyecto el viernes
+                        break;
+                    case "~":
+                        par11 = new Expression(parameterList[j + 1].Value);
+                        par1 = par11.Evaluate();
+                        if ((int)par1 == 0)
+                        {
+                            result.Add(1);
+                        }
+                        else result.Add(0);
+                        //Voy a usar NCalc para realizar esto, integrar al proyecto el viernes
+                        break;
+                    case "|":
+                        par11 = new Expression(parameterList[j - 1].Value);
+                        par1 = par11.Evaluate();
+                        par22 = new Expression(parameterList[j + 1].Value);
+                        par2 = par22.Evaluate();
+                        if (((int)par1 == 1) || ((int)par2 == 1))
+                        {
+                            result.Add(1);
+                        }
+                        else result.Add(0);
+                        break;
+                    case "&":
+                        par11 = new Expression(parameterList[j - 1].Value);
+                        par1 = par11.Evaluate();
+                        par22 = new Expression(parameterList[j + 1].Value);
+                        par2 = par22.Evaluate();
+                        if (((int)par1 == 1) && ((int)par2 == 1))
+                        {
+                            result.Add(1);
+                        }
+                        else result.Add(0);
+                        break;
+                    default:
+                        //Catch exception, error
+                        break;
+                }
+            }
+        }
+        if (result.Count > 1)
+        {
+            int temp = 1;
+            for (int k = 0; k < result.Count; k++)
+            {
+                if (result[k] == 0) temp = 0;
+            }
+            return temp;
+        }
+        else return result[0];
     }
 }
